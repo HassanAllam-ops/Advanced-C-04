178bbbee134b5c850e5f8160e28d165064e5f8a3
Hassan Allam <hassan309pro@gmail.com>
2025-08-06 22:35:17 +0300

    Q1 : Given an array of integers, count the frequency of each element using a hash table.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Advanced_C__04
+﻿using System.Collections;
+
+namespace Advanced_C__04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Q1
+            //int[] numbers = { 1, 2, 3, 2, 3, 1, 1, 4, 5, 6, 1, 2, 2, 3 };
+            //
+            //Hashtable FrequencyTable = new Hashtable();
+            //
+            //foreach (int num in numbers)
+            //{
+            //    if (FrequencyTable.ContainsKey(num))
+            //    {
+            //        FrequencyTable[num] = (int)FrequencyTable[num] + 1;
+            //    }
+            //    else
+            //    {
+            //        FrequencyTable[num] = 1;
+            //    }
+            //}
+            //
+            //foreach (DictionaryEntry entry in FrequencyTable)
+            //{
+            //    Console.WriteLine($"Element {entry.Key} Occurs {entry.Value} Time(s).");
+            //}
+            #endregion
+
         }
     }
 }
